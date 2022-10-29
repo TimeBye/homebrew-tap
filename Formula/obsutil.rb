f4b11446683069f814b7652d54e718bad29348c7
TimeBye <setzero@aliyun.com>
2022-10-29 13:52:48 +0800

    [IMP]增加obsutil

--- Formula/obsutil.rb
+++ Formula/obsutil.rb
@@ -1,13 +1,11 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class AliyundriveCli < Formula
-  desc "阿里云盘命令行客户端"
-  version "0.13.0"
-  homepage "https://github.com/chyroc/aliyundrive-cli"
-  url "https://ghproxy.com/github.com/chyroc/aliyundrive-cli/releases/download/v0.13.0/aliyundrive-cli_0.13.0_Darwin_x86_64.tar.gz"
-  sha256 "0a61cf44ae80ebc0c578f765e160e3effb8f28b4d24d67c41086901cf7f39dbd"
-  license "Apache-2.0"
+class Obsutil < Formula
+  desc "obsutil是一款用于访问管理华为云对象存储服务（Object Storage Service，OBS）的命令行工具"
+  homepage "https://support.huaweicloud.com/utiltg-obs/obs_11_0003.html"
+  url "https://ghproxy.com/github.com/TimeBye/homebrew-tap/raw/artifacts/obsutil_darwin_amd64_5.4.6.tar.gz"
+  version "5.4.6"
 
   # depends_on "cmake" => :build
 
@@ -17,7 +15,7 @@ class AliyundriveCli < Formula
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    bin.install "aliyundrive-cli"
+    bin.install "obsutil"
   end
 
   test do
@@ -25,11 +23,11 @@ class AliyundriveCli < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test aliyundrive-cli`. Options passed
+    # software. Run the test with `brew test obsutil`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "aliyundrive-cli -version"
+    system "obsutil version"
   end
 end
